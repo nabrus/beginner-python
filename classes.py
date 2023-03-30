a9de9fc5f3ef4efe77d965dbf9cf392eed7b95ca
butters <34988577+nabrus@users.noreply.github.com>
2023-03-29 22:29:43 -0400

    Add basic class with instances

--- classes.py
+++ classes.py
@@ -1,7 +1,17 @@
 # A Class is like an object constructor, or a "blueprint" for creating objects.
 class Cat:
-    def speak(self):
+    def __init__(self, name, age):  # a constructor
+        self.name = name
+        self.age = age
+
+    def speak(self):  # Created a method
         print("meow!")
 
 
-)
+# mose = Cat()  # Creates an instance of a class which is an object
+# print(type(mose))  # <class '__main__.Cat'>
+
+mose = Cat("Mose", 1006)
+print(mose.name)  # Mose
+print(mose.age)  # 1006
+mose.speak()  # meow! note-not wrapped in print because method uses print, doesn't return anything
