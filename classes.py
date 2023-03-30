6c3448af110ede9bbb52cb159575a6288cf05877
butters <34988577+nabrus@users.noreply.github.com>
2023-03-29 22:42:34 -0400

    Complete classes overview

--- classes.py
+++ classes.py
@@ -15,3 +15,25 @@ mose = Cat("Mose", 1006)
 print(mose.name)  # Mose
 print(mose.age)  # 1006
 mose.speak()  # meow! note-not wrapped in print because method uses print, doesn't return anything
+
+# Inheritance
+
+
+class Animal:
+    def walk(self):
+        print("Walking...")
+
+
+class Dog(Animal):  # Dog class inherits the Animal class
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def speak(self):
+        print("Woof!")
+
+
+sandy = Dog("Sandy", 2459)
+print(sandy.age)  # 2459
+sandy.speak()  # Woof!
+sandy.walk()  # Walking...
