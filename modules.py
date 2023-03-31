f8c1f66e452d6ad57f40909f619bb3b959d662f1
butters <34988577+nabrus@users.noreply.github.com>
2023-03-30 22:33:52 -0400

    Add standard lib comment

--- modules.py
+++ modules.py
@@ -16,4 +16,17 @@ cat.speak()  # Meeeeeooooooww!
 
 dog.growl()  # Grrrrrrr
 
-#
+# Python Standard Library
+# A huge collection of utilities
+
+# math for math utilities
+# re for regular expressions
+# json to work with JSON
+# datetime to work with dates
+# sqlite3 to use SQLite
+# os for Operating System utilities
+# random for random number generation
+# statistics for statistics utilities
+# requests to perform HTTP network requests
+# http to create HTTP servers
+# urllib to manage URLs
