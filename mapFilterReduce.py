ca784c1f49a08d1290cb775492dcbb7026a59c69
butters <34988577+nabrus@users.noreply.github.com>
2023-04-02 18:19:05 -0400

    Add map file and start examples

--- mapFilterReduce.py
+++ mapFilterReduce.py
@@ -1,1 +1,17 @@
 # Global functions - map(), filter(), reduce()
+
+# The `map()` function executes a specified function for each item in an iterable. The item is sent to the function as a parameter.
+# Syntax: map(function, iterables)
+numbers = [1, 2, 3]
+
+
+def double(a):
+    return a * 2
+
+
+result = map(double, numbers)
+
+print(list(result))  # [2, 4, 6]
+
+# Convert to a `lambda` function
+`
