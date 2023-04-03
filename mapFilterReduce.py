54d2c4dccf17ad7565816fb09d0d55e5eacb1e71
butters <34988577+nabrus@users.noreply.github.com>
2023-04-02 21:41:41 -0400

    Complete map function examples

--- mapFilterReduce.py
+++ mapFilterReduce.py
@@ -13,5 +13,16 @@ result = map(double, numbers)
 
 print(list(result))  # [2, 4, 6]
 
-# Convert to a `lambda` function
-`
+# Use `lambda` function
+
+numbers_2 = [4, 5, 6]
+# Note: Always use a def statement instead of an assignment statement that binds a lambda expression directly to an identifier. per PEP8 style guide
+
+# double_2 = lambda a: a * 2
+# result_2 = map(double_2, numbers_2)
+# print(list(result_2))  # [8, 10, 12]
+
+# Acceptable way of using lambda
+numbers_3 = [7, 8, 9]
+result_3 = map(lambda a: a * 2, numbers_3)
+print(list(result_3))  # [14, 16, 18]
