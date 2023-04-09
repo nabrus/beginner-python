ed63c63a166df5a74d5463cbb546c6b8796a7d97
butters <34988577+nabrus@users.noreply.github.com>
2023-04-08 23:43:16 -0400

    Add decorator file with definitions

---
+++ decorator.py
@@ -0,0 +1,4 @@
+# A decorator is a function that takes another function and extends the behavior of the latter function without explicitly modifying it.
+# In Python, functions are first-class objects. This means that functions can be passed around and used as arguments, just like any other object (string, int, float, list, and so on).
+
+# Put simply: decorators wrap a function, modifying its behavior
