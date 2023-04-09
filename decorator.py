1cf6327f185638353b2389777388a6b1ac90d81a
butters <34988577+nabrus@users.noreply.github.com>
2023-04-09 12:28:02 -0400

    Add syntax comment

--- decorator.py
+++ decorator.py
@@ -2,3 +2,5 @@
 # In Python, functions are first-class objects. This means that functions can be passed around and used as arguments, just like any other object (string, int, float, list, and so on).
 
 # Put simply: decorators wrap a function, modifying its behavior
+
+# Python allows you to use decorators in a simpler way with the @ symbol, sometimes called the “pie” syntax.
