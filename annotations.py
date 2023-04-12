4dad906afbb5724c292b56cefc27133c75ae2a3f
butters <34988577+nabrus@users.noreply.github.com>
2023-04-12 12:19:42 -0400

    Add file and comments with definition

---
+++ annotations.py
@@ -0,0 +1,2 @@
+# Python is a dynamically-typed language which means that you don’t have to declare the data type of the variable beforehand.
+# Annotations are Python features that hint developers about the data types of the variables or function parameters and return type.
