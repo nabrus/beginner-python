cfa669a1ea953922dfb431a8f3f4aee64e94a0f3
butters <34988577+nabrus@users.noreply.github.com>
2023-04-12 13:23:36 -0400

    Complete user input example

---
+++ userInput.py
@@ -0,0 +1,4 @@
+# print("What is your age?") or put in the input func
+age = input("What is your age? ")
+print(f"Your age is {age}.")
+# The input is at runtime, the program stops execution until received.
