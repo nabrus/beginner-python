41492862b349921e28568d1d9595bbe158bfeb5a
butters <34988577+nabrus@users.noreply.github.com>
2023-04-11 22:43:51 -0400

    Complete decorator example

--- decorator.py
+++ decorator.py
@@ -1,6 +1,26 @@
-# A decorator is a function that takes another function and extends the behavior of the latter function without explicitly modifying it.
+# Decorators - change, enhance, or alter how a function  works.
 # In Python, functions are first-class objects. This means that functions can be passed around and used as arguments, just like any other object (string, int, float, list, and so on).
+# A decorator is a function that takes another function and extends the behavior of the latter function without explicitly modifying it.
 
 # Put simply: decorators wrap a function, modifying its behavior
 
-# Python allows you to use decorators in a simpler way with the @ symbol, sometimes called the “pie” syntax.
+# Python syntax: you to use decorators with the @ symbol, sometimes called the “pie” syntax.
+
+def log_time(func):
+    def wrapper():
+        print("do something before")
+        val = func()
+        print("do something after")
+        return val
+    return wrapper
+
+
+@log_time
+def hello():
+    print("Hello!")
+
+
+hello()
+# do something before
+# Hello!
+# do something after
