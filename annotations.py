656e23886dfdbdf5a0c0a58fcaf29ed9b002abd6
butters <34988577+nabrus@users.noreply.github.com>
2023-04-12 22:39:34 -0400

    Complete annotations examples

--- annotations.py
+++ annotations.py
@@ -1,2 +1,10 @@
 # Python is a dynamically-typed language which means that you don’t have to declare the data type of the variable beforehand.
 # Annotations are Python features that hint developers about the data types of the variables or function parameters and return type.
+
+# Specifying this func receives an integer and returns an integer
+def increment(n: int) -> int:
+    return n + 1
+
+
+# Specify a variable is an integer
+count: int = 0
