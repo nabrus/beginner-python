b519396b333c7edf5a26681b28ba01faf8c3f616
butters <34988577+nabrus@users.noreply.github.com>
2023-04-14 23:43:55 -0400

    Add and complete polymorphism example

---
+++ polymorphism.py
@@ -0,0 +1,15 @@
+class Dog:
+    def eat(self):
+        print("Eating dog food, crunch crunch crunch")
+
+
+class Cat:
+    def eat(self):
+        print("Eating cat food, num num num")
+
+
+animal_1 = Dog()
+animal_2 = Cat()
+
+animal_1.eat()
+animal_2.eat()
