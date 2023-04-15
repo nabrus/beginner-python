b49922c8d97ccda696b692ffc9e617989d456f10
butters <34988577+nabrus@users.noreply.github.com>
2023-04-14 21:11:31 -0400

    Add pip file with basic pip commands

---
+++ pip.py
@@ -0,0 +1,14 @@
+# Third party packages - pip: the standard package manager for Python
+# The list of available packages can be found at pypi.org
+
+# `pip install <package name>`
+# Installs the package globally
+
+# `pip show <package name>`
+# Shows info about the package
+
+# `pip install -U <package name>`
+# Installs latest version of that package
+
+# `pip uninstall <package name>`
+# Uninstalls package
