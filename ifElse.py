3697799a178867fe257f7b6837c1e2b59a767a28
butters <34988577+nabrus@users.noreply.github.com>
2023-04-15 16:17:00 -0400

    Add and complete if else statement example

---
+++ ifElse.py
@@ -0,0 +1,16 @@
+# Control Flow - `if`, `else` `elif` statements
+condition = False
+name = "Wyatt"
+
+if condition == True:
+    print("The condition")
+    print("was True")
+elif name == "Richard":
+    print("Hello Richard!")
+elif name == "Wyatt":
+    print("Hello Wyatt!!")  # Hello Wyatt!!
+elif name == "Henry":
+    print("Hello Henry!")
+else:
+    print("The condition")
+    print("was False")
