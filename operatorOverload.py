15c634cbaf49f504fe9bd4e2d807634b6fafd72d
butters <34988577+nabrus@users.noreply.github.com>
2023-04-16 16:40:48 -0400

    Add to comment

--- operatorOverload.py
+++ operatorOverload.py
@@ -1,5 +1,7 @@
 # Operator Overloading
 # In Python, operator overloading allows you to define how operators such as +, -, *, /, ==, !=, <, >, and others behave for instances of your custom classes.
+# When you overload an operator, you define what should happen when that operator is used with instances of your class. This
+# can be useful if you want to make your class behave like a built-in data type, or if you want to define a custom behavior for a particular operator.
 class Dog:
     # the dog class
     def __init__(self, name, age):
